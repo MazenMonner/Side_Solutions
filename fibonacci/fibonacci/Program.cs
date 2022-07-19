bacca515a4b0e19fabd396d520874cd575c46371
mazen_moneer <54606829+MazenMonner@users.noreply.github.com>
2022-07-19 16:54:46 +0200

    Add files via upload

---
+++ fibonacci/fibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int y = 0;
+            int z ;
+            Console.Write("input the n :  ");
+            int n = Convert.ToInt32(Console.ReadLine()); 
+            for (int i = 0; i < n; i++)
+            {
+                z = x + y;
+                x = y;
+                y = z;
+                Console.WriteLine(z);
+            }
+
+
+        }
+    }
+}
